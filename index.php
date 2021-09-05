5f7b56ff40ad9da92e3cd03e19b455afa53e13b3
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2021-09-05 19:58:46 -0300

    Uploading the first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Dev</title>
+</head>
+
+<body>
+    <div class="Container--title">
+        <h1>Descubra Qual tipo de dev vc quer ser </h1>
+    </div>
+    <form>
+        <div>
+
+        </div>
+    </form>
+
+</body>
+
+</html>
