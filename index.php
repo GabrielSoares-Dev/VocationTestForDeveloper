7585f9ef82f7b9af39c36b34feb13b19384a6ba2
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2021-09-05 21:31:51 -0300

    up first

--- index.php
+++ index.php
@@ -10,10 +10,33 @@
 
 <body>
     <div class="Container--title">
-        <h1>Descubra Qual tipo de dev vc quer ser </h1>
+        <h1 align='center'>Descubra qual o seu tipo de dev </h1>
     </div>
-    <form>
-        <div>
+    <form method="POST" action="">
+        <div class="container-form">
+            <div class="container--p1">
+                <h4> 1-Com o que vc gosta mais de trabalhar</h4>
+                <div>
+                    <input type='checkbox' value='dados' name='p1' />
+                    <label>Dados</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='interface' name='p1' />
+                    <label>Criar interface</label>
+                </div>
+                <div class="container--p2">
+                <h4> 2-Qual a sua preocupação?</h4>
+                <div>
+                    <input type='checkbox' />
+                    <label>Experiencia do cliente</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='exp' name='p2' />
+                    <label></label>
+                </div>
+
+
+            </div>
 
         </div>
     </form>
